54ac8e7bcee6a59d6cd7aa347fb7dd66a3576311
Chloe <cehan16@gmail.com>
2016-10-24 16:47:25 -0400

    update main activity

--- app/src/main/java/com/example/chloe/timedquizapp/MainActivity.java
+++ app/src/main/java/com/example/chloe/timedquizapp/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.chloe.timedquizapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity implements OnClickListener{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonQM = (Button) findViewById(R.id.btnQM);
+        buttonQM.setOnClickListener(this);
+
+        Button buttonQT = (Button) findViewById(R.id.btnQT);
+        buttonQT.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnQM){
+            Intent homeQM = new Intent(this, QMHome.class);
+            startActivity(homeQM);
+        }
+        else if (v.getId() == R.id.btnQT){
+            Intent loginQT = new Intent(this, QTLogin.class);
+            startActivity(loginQT);
+        }
+        else{
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/chloe/timedquizapp/QMHome.java
@@ -0,0 +1,8 @@
+package com.example.chloe.timedquizapp;
+
+/**
+ * Created by Chloe on 2016-10-24.
+ */
+
+public class QMHome {
+}
